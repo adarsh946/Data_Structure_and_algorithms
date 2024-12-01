4577017bfab94b3c17ac852e34d25d78d0ccbd62
adarsh946 <adarsh.shukla2724@gmail.com>
2024-12-02 01:14:33 +0530

    patterns

---
+++ patterns/patterns.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    // int n =5;
+    // for (int i =1 ; i <=n; i ++){
+    //     for (int j = 1; j<=n ; j++){
+    //       cout << j;
+    //     }
+    //    cout << endl;
+    // }
+
+    // output : 
+                //  12345
+                //  12345
+                //  12345
+                //  12345
+                //  12345
+    
+
+    // int n = 3;
+    // int num = 1;
+    // for(int i =0; i<n; i++){
+    //     for(int j =0; j< n ; j++){
+    //         cout << num << " ";
+    //         num = num + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // output : 1 2 3 
+    //          4 5 6
+    //          7 8 9
+
+    // int n = 3; 
+    // char ch = 'A';
+    // for (int i = 0; i<n; i++){
+    //     for(int j = 0; j<n; j++){
+    //         cout << ch << " ";
+    //         ch = ch + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    //  output :   A B C 
+    //             D E F
+    //             G H I
+
+
+    // int n = 3;
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < i+1; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // output : *
+    //          **
+    //          ***
+    
+    return 0;
+    }
