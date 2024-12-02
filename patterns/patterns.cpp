1643e48547d45d093568a9b16c1e8d1ec93280a5
adarsh946 <adarsh.shukla2724@gmail.com>
2024-12-02 11:50:09 +0530

    more patterns

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -60,6 +60,153 @@ int main(){
     // output : *
     //          **
     //          ***
+
+   
+   
+   
+    // int n =4;
+    // int num =0;
     
+    // for(int i = 0; i < n; i++){
+    //         num++;
+    //     for(int j = 0; j < i + 1; j++){
+    //         cout << num  ;
+            
+    //     }
+    //     cout << endl;
+    // }
+
+    // output: 1
+    //         22
+    //         333
+    //         4444
+
+
+    
+    // int n=4;
+    
+    // for(int i =0 ; i< n ; i++){
+    //     int num = 1;
+    //     for(int j = 0; j < i+1; j++ ){
+    //        cout << num;
+    //        num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // output: 1
+    //         12
+    //         123
+    //         1234            
+
+
+
+    // int n=5;
+    // for(int i = 0; i<n; i++){
+    //     for(int j =i+1; j> 0; j--){
+    //         cout << j;
+            
+    //     }
+    //     cout << endl;
+    // }
+
+    // output : 1
+    //          21
+    //          321
+    //          4321
+    //          54321             
+    
+
+    
+
+    // int n = 6;
+    // int num = 0;
+    // for(int i =0; i<n; i++){
+    //     num ++;
+    //     for(int j = n; j > i; j--){
+    //        cout << num;
+           
+    //     }
+    //     cout << endl;
+    // }
+
+    // output : 111111
+    //          22222
+    //          3333
+    //          444
+    //          55
+    //          6              
+
+
+
+    // int n =4; 
+    // for(int i =0; i< n; i++){
+    //     for(int j = (n-i-1) ; j > 0; j--){
+    //         cout << " ";
+           
+    //     }
+    //     for(int k = 0; k < i+1; k++){
+    //         cout << k+1;
+    //        }
+    //        for(int l = i; l > 0; l--){
+    //         cout << l;
+            
+    //        }
+    //     cout << endl;
+    // }
+
+    // output :   1
+    //           121
+    //          12321
+    //         1234321
+
+
+
+
+// int n =4; 
+  
+//     // Top
+// for(int i =0; i < n; i++){
+//     for(int j =0; j< (n-i-1); j++){
+//         cout << " ";
+//     }
+//     cout << "*";
+//     if(i != 0){
+//         for(int k = 0; k< (2*i -1); k++){
+//             cout << " ";
+//         }
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+   
+//    // Bottom
+// for(int i = 0; i< n-1; i++){
+//     for(int j = 0; j < i+1; j++){
+//         cout << " ";
+
+//     }
+//     cout << "*";
+//     if(i !=n-2){
+//         for(int k =0; k < 2*(n-2-i)-1; k++){
+//             cout << " ";
+//         }
+//         cout << "*";
+//     }
+//     cout << endl;
+
+// }
+
+// output:   *
+//          * *
+//         *   *
+//        *     *
+//         *   *
+//          * *
+//           *    
+
+
+
+
     return 0;
     }
